fad61da4661da1efb708125362b3b0c168b3e058
unknown <hritik.tiwari_cs16@gla.ac.in>
2018-10-13 21:13:07 +0530

    hi

--- programone.py
+++ programone.py
@@ -1,7 +1,17 @@
-na=int(input())
-if na==0:
-    print("Zero")
-elif na>0:
-    print("positive")
+n=input()
+nn=list(n)
+#print(nn)
+c=0
+for x in nn:
+    if 97<=ord(x)<=122 or 65<=ord(x)<=90 or 32<=ord(x)<=42 or 58<=ord(x)<=64 or  91<=ord(x)<=96 or 123<=ord(x)<=127 or ord(x)==47 or ord(x)==44:
+        c+=1
+if c==0:
+    n=float(n)
+    if n==0:
+        print("zero")
+    elif n>0:
+        print("positive")
+    else:
+        print("negative")
 else:
-    print("negative")
+    print("invalid input")
