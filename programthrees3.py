41322f6caf4dcf19dd1a3981e90cb8dc7f700e75
hritik1330 <hritik.tiwari_cs16@gla.ac.in>
2018-11-25 11:30:29 +0530

    hhhh

--- programthrees3.py
+++ programthrees3.py
@@ -2,18 +2,18 @@ n=input()
 l=list(n)
 z=[]
 c=0
-for p in l:
-    if 48<=ord(p)<=57:
+for a in l:
+    if 48<=ord(a)<=57:
         c+=1
 if c==len(l):
     for x in range(int(n)):
         z.append(int(input()))
-    m=z[0]
+    p=z[0]
     for x in range(len(z)):
-        if z[x]<m:
-            z[x],m=m,z[x]
+        if z[x]<p:
+            z[x],p=p,z[x]
         else:
-            m=m
-    print(m)
+            p=p
+    print(p)
 else:
     print("invalid input")
